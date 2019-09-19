6e524e59aff403d8e0d867a8d5bc79e07f8e1c70
Tiberio D A R Ferreira <tiberiusferreira@gmail.com>
2019-09-19 10:07:07 -0300

    print bounding box

--- src/image.c
+++ src/image.c
@@ -289,6 +289,8 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
             if(right > im.w-1) right = im.w-1;
             if(top < 0) top = 0;
             if(bot > im.h-1) bot = im.h-1;
+            //printf("Detected %s with confidence %.0f%%\n", names[j], dets[i].prob[j]*100);
+            printf("Bounding Box: Left=%d, Top=%d, Right=%d, Bottom=%d\n", left, top, right, bot);
 
             draw_box_width(im, left, top, right, bot, width, red, green, blue);
             if (alphabet) {
